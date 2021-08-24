f7ffd43c520bbdfd1e7ca12ca48ed4b3964bc7f7
Nedson <nedsonsa@gmail.com>
2021-08-24 00:46:42 -0300

    Burger is now fixed when menu is active

--- js/navbar.js
+++ js/navbar.js
@@ -4,6 +4,9 @@ const menu = document.querySelector('.wrapper')
 function toggleMenu() {
    hamburguerElement.classList.toggle('animate')
    menu.classList.toggle('active')
+   menu.classList.contains('active')
+      ? this.classList.add('fixed')
+      : this.classList.remove('fixed')
 }
 
 hamburguerElement.addEventListener('click', toggleMenu)
